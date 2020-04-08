0b05b3e8140968b128ffe58997c0b3af480928b3
Celso Alves <celsodesign@gmail.com>
2020-04-08 15:56:34 -0300

    Created the schema

---
+++ apollo/utils/schema.js
@@ -0,0 +1,9 @@
+const { makeExecutableSchema } = require('graphql-tools');
+
+const typeDefs = require('../typeDefs');
+const resolvers = require('../resolvers');
+
+module.exports = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
